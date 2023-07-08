c5858a1b87e173df68b2b889c5eda7e5409e3480
Rushabh Gohel <103501258+Rushabh378@users.noreply.github.com>
2023-07-08 17:02:40 +0530

    adding gems class

---
+++ Assets/Scripts/Currancy/Gems.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace ChestSystem.Currancy
+{
+    [Serializable]public class Gems : ICurrancy
+    {
+        public int Amount;
+
+        public void add(int value)
+        {
+            Amount += value;
+        }
+
+        public int get()
+        {
+            return Amount;
+        }
+    }
+}
