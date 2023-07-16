69d42babfc89b3d9f50d79f570f20a733a4bebd0
Rushabh Gohel <103501258+Rushabh378@users.noreply.github.com>
2023-07-16 12:00:41 +0530

    added functionality to currancy

--- Assets/Scripts/Currancy/Currancy.cs
+++ Assets/Scripts/Currancy/Currancy.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using UnityEngine;
+using ChestSystem.UI;
 
 namespace ChestSystem.Currancy
 {
@@ -7,5 +8,20 @@ namespace ChestSystem.Currancy
     {
         [SerializeReference]public ICurrancy coins = new Coins();
         [SerializeReference]public ICurrancy gems = new Gems();
+
+        private UpdateCurrancy[] updaters;
+
+        public void Start()
+        {
+            updaters = GetComponentsInChildren<UpdateCurrancy>();
+        }
+
+        public void UpdateCurrancy()
+        {
+            foreach(UpdateCurrancy updater in updaters)
+            {
+                updater.Refresh();
+            }
+        }
     }
 }
